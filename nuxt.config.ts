aefe3787a085ac029c39ba79043bbfc8670d47de
YesseniaColocho <Colocho.yessenia@gmail.com>
2024-09-18 22:57:44 +0200

    First deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@hypernym/nuxt-gsap']
+  modules: ['@hypernym/nuxt-gsap'],
+  buildDir: 'docs'
+
 })
